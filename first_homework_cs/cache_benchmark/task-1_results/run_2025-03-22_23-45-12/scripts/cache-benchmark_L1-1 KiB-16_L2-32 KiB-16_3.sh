c3cbccb3adc3ea4cc415002ff0bcfca4382d0090
Simon Goričar <simon.peter.goricar@gmail.com>
2025-03-23 15:56:41 +0100

    Add results for cache benchmark (task 1/2)

---
+++ first_homework_cs/cache_benchmark/task-1_results/run_2025-03-22_23-45-12/scripts/cache-benchmark_L1-1 KiB-16_L2-32 KiB-16_3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --reservation=fri
+#SBATCH --job-name=rs-cache-perf_egNYP5
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --output="/d/hpc/home/sg7710/rs/rs-dn1/cache_benchmark/results/run_2025-03-22_23-45-12/scripts/cache-benchmark_L1-1 KiB-16_L2-32 KiB-16_3.log"
+#SBATCH --time=00:18:00
+
+GEM5_WORKSPACE=/d/hpc/projects/FRI/GEM5/gem5_workspace
+GEM5_ROOT=$GEM5_WORKSPACE/gem5
+GEM_PATH=$GEM5_ROOT/build/X86
+
+srun apptainer exec $GEM5_WORKSPACE/gem5.sif $GEM_PATH/gem5.opt \
+    --outdir="/d/hpc/home/sg7710/rs/rs-dn1/cache_benchmark/results/run_2025-03-22_23-45-12/benchmarks/L1-1 KiB-16_L2-32 KiB-16_3" cache_benchmark.py \
+        --l1_size="1 KiB" --l2_size="32 KiB" \
+        --l1_assoc="16" --l2_assoc="16" \
+        --mult_version="3"
+
---
+++ first_homework_cs/cache_benchmark/task-1_results/run_2025-03-22_23-45-12/scripts/cache-benchmark_L1-8 KiB-16_L2-32 KiB-16_2.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+#SBATCH --reservation=fri
+#SBATCH --job-name=rs-cache-perf_GZ/yLq
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=1
+#SBATCH --output="/d/hpc/home/sg7710/rs/rs-dn1/cache_benchmark/results/run_2025-03-22_23-45-12/scripts/cache-benchmark_L1-8 KiB-16_L2-32 KiB-16_2.log"
+#SBATCH --time=00:18:00
+
+GEM5_WORKSPACE=/d/hpc/projects/FRI/GEM5/gem5_workspace
+GEM5_ROOT=$GEM5_WORKSPACE/gem5
+GEM_PATH=$GEM5_ROOT/build/X86
+
+srun apptainer exec $GEM5_WORKSPACE/gem5.sif $GEM_PATH/gem5.opt \
+    --outdir="/d/hpc/home/sg7710/rs/rs-dn1/cache_benchmark/results/run_2025-03-22_23-45-12/benchmarks/L1-8 KiB-16_L2-32 KiB-16_2" cache_benchmark.py \
+        --l1_size="8 KiB" --l2_size="32 KiB" \
+        --l1_assoc="16" --l2_assoc="16" \
+        --mult_version="2"
+
